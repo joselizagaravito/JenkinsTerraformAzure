4409dee4cd594a6dd81692d3ac45e630a1bcaae5
jose liza <joselizagaravito@gmail.com>
2024-12-14 02:11:22 -0500

    Actualizacion de acceso a Azure

--- infra/variables.tf
+++ infra/variables.tf
@@ -5,3 +5,7 @@ variable "location" {
 variable "resource_group_name" {
   default = "jenkins-aks-rg"
 }
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {}
+variable "tenant_id" {}
